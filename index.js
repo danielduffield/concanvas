b57acbb8dc58d7f736485b807382f08c4bed50e8
Daniel Duffield <danieljduffield@gmail.com>
2017-08-08 10:45:59 -0700

    Created a web server to listen on designated port for requests. #1

---
+++ index.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(process.env.PORT, () => console.log('Listening on PORT...'))
