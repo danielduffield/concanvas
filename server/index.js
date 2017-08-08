cb9666aa6500ea6cafcd547e53d372fe88224b2e
Daniel Duffield <danieljduffield@gmail.com>
2017-08-08 10:50:56 -0700

    Created a server folder to house index.js and public folder, which is served as static middleware. #1

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.use(express.static('server/public'))
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(process.env.PORT, () => console.log('Listening on PORT...'))
