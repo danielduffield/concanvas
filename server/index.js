edf9661367b7a721f3029b17c9cb392369c58f1d
Daniel Duffield <danieljduffield@gmail.com>
2017-08-09 14:20:10 -0700

    Added a function to delete files from the bottom of the stack each time a new file is successfully submitted. #3

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ const jsonParser = bodyParser.json()
 
 const fs = require('fs')
 
+let currentCanvas = null
+
 server.listen(process.env.PORT, () => console.log('Listening on PORT...'))
 
 app.use(jsonParser)
@@ -18,14 +20,29 @@ app.use(express.static('server/public'))
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/saved-canvas', (req, res) => {
-  readFileAsync('canvas-state/last-saved.json').then(data => {
-    res.json(data)
+  let canvasPath = 'canvas-state/blank.json'
+  readdirAsync('canvas-state/instances').then(data => {
+    if (data.length) {
+      canvasPath = 'canvas-state/instances/' + data[data.length - 1]
+    }
+    readFileAsync(canvasPath).then(data => {
+      res.json(data)
+    })
   })
   .catch(err => res.status(500).json(err))
 })
 
 app.post('/', (req, res) => {
-  writeFileAsync('canvas-state/saved-canvas-' + Date.now() + '.json', JSON.stringify(req.body))
+  readdirAsync('canvas-state/instances').then(data => {
+    console.log(data)
+    currentCanvas = data[data.length - 1]
+    console.log(currentCanvas)
+    const toDelete = data.shift()
+    unlinkAsync('canvas-state/instances/' + toDelete).then(() => {
+      console.log('DELETED ', toDelete)
+    })
+  })
+  writeFileAsync('canvas-state/instances/canvas-' + Date.now() + '.json', JSON.stringify(req.body))
     .then(() => {
       console.log('SAVED!')
       res.sendStatus(201)
@@ -59,6 +76,24 @@ function writeFileAsync(fileName, data) {
     fs.writeFile(fileName, data, 'utf8', (err) => {
       if (err) return reject(err)
       else return resolve()
+    })
+  })
+}
+
+function readdirAsync(path) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(path, (err, files) => {
+      if (err) return reject(err)
+      return resolve(files)
+    })
+  })
+}
+
+function unlinkAsync(path) {
+  return new Promise((resolve, reject) => {
+    fs.unlink(path, (err) => {
+      if (err) return reject(err)
+      return resolve()
     })
   })
 }
