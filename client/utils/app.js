d341ec89ce807c7d42ee9e4a6ec206b4cd122c77
Daniel Duffield <danieljduffield@gmail.com>
2017-08-12 14:31:31 -0700

    Changed the components rendered in app to switch from PaintToolbar to PaintSidebar. #9

--- client/utils/app.js
+++ client/utils/app.js
@@ -3,11 +3,27 @@ import React from 'react'
 import Canvas from './canvas'
 import ChatSidebar from './chat-sidebar'
 
+import styled from 'styled-components'
+
 export default function App() {
   return (
     <div>
+      <MainTitle id="main-title">ConCanvas</MainTitle>
       <ChatSidebar />
       <Canvas />
     </div>
   )
 }
+
+const MainTitle = styled.h1`
+  width: 300px;
+  top: 10px;
+  font-size: 4em;
+  font-family: 'Bubblegum Sans', cursive;
+  left: 0;
+  right: 0;
+  position: absolute;
+  margin: 0 auto;
+  background-color: whitesmoke;
+  z-index: 10;
+`
