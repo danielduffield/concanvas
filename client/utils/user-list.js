196b80f670922392a7a18adacd37068817de18e7
Daniel Duffield <danieljduffield@gmail.com>
2017-08-15 11:32:30 -0700

    Added styling to the UserList component and set the div containing the actual list to have an overflow property set to 'scroll'. #11

--- client/utils/user-list.js
+++ client/utils/user-list.js
@@ -2,8 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 import { connect } from 'react-redux'
 
-import store from './store'
-
 class UserList extends React.Component {
   constructor(props) {
     super(props)
@@ -18,9 +16,16 @@ class UserList extends React.Component {
   render() {
     return (
       <div>
-        <UnhideUsers className="chat-button"
+        <UnhideUsers className={this.props.isUserListHidden ? 'chat-button' : 'chat-button hidden'}
           onClick={this.toggleUserList}>Display Users</UnhideUsers>
-        <UsersContainer className={this.props.isUserListHidden ? 'hidden' : ''}></UsersContainer>
+        <UsersContainer className={this.props.isUserListHidden ? 'hidden' : ''}>
+          <UsersTopBar>
+            <HideUsers id="chat-hide" className="float-left"
+              onClick={this.toggleUserList}>Hide Users</HideUsers>
+            <UserCount>Online: </UserCount>
+          </UsersTopBar>
+          <Users></Users>
+        </UsersContainer>
       </div>
     )
   }
@@ -33,6 +38,24 @@ const UnhideUsers = styled.button`
   width: 200px;
   height: 50px;
   z-index: 10;
+  margin: 0;
+`
+
+const HideUsers = styled.button`
+  height: 100%;
+  width: 25%;
+  margin-left: 5%;
+  border-radius: 5px;
+  min-width: 90px;
+  font-size: 1em;
+`
+
+const UserCount = styled.div`
+  float: left;
+  height: 100%;
+  line-height: 200%;
+  margin-left: 5%;
+  background-color: grey;
 `
 
 const UsersContainer = styled.div`
@@ -41,6 +64,21 @@ const UsersContainer = styled.div`
   height: 40%;
   background-color: grey;
   z-index: 5;
+`
+
+const UsersTopBar = styled.div`
+  position: absolute;
+  height: 7%;
+  width: 100%;
+  background-color: grey;
+`
+
+const Users = styled.div`
+  background-color: lightgrey;
+  height: 90%;
+  width: 90%;
+  margin: 5%;
+  overflow-y: scroll;
 `
 
 function mapStateToProps(state) {
