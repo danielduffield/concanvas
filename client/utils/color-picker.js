0dfa214205d492c0fb235eb24b11ff77fe9d457c
Daniel Duffield <danieljduffield@gmail.com>
2017-08-16 11:05:12 -0700

    Added a dispatch that reveals the ColorPicker on click of the custom color modules. #13

--- client/utils/color-picker.js
+++ client/utils/color-picker.js
@@ -1,12 +1,12 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { SketchPicker } from 'react-color'
 import styled from 'styled-components'
 
-export default class Component extends React.Component {
-
+class Component extends React.Component {
   render() {
     return (
-      <CustomColor>
+      <CustomColor isColorPickerHidden={this.props.isColorPickerHidden}>
         <SketchPicker />
       </CustomColor>
     )
@@ -18,4 +18,14 @@ const CustomColor = styled.div`
   top: 428px;
   right: 357px;
   float: left;
+  display: ${props => props.isColorPickerHidden ? 'none' : ''};
 `
+
+function mapStateToProps(state) {
+  return {
+    isColorPickerHidden: state.paint.isColorPickerHidden
+  }
+}
+
+const Connected = connect(mapStateToProps)(Component)
+export default Connected
