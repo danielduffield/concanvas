ce081409c5e431eed3cdddf2f924f02d18c8351f
Daniel Duffield <danieljduffield@gmail.com>
2017-11-06 14:59:20 -0800

    Added toggleChat method for handling onClick of chat button and added styling to toolbar divs. #23

--- client/utils/util-toolbar.js
+++ client/utils/util-toolbar.js
@@ -5,22 +5,44 @@ import { connect } from 'react-redux'
 import DownloadModule from './download-module'
 
 class UtilToolbar extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.toggleChat = this.toggleChat.bind(this)
+  }
+  toggleChat() {
+    this.props.dispatch({ type: 'TOGGLED_CHAT' })
+  }
   render() {
     return (
-      <div>
-        <DownloadModule canvas={this.props.canvas}/>
-        <ToggleButton id="unhide-button" className="toggle-button"
-          isActive={!(this.props.isChatHidden && this.props.isUserListHidden)}
-          onClick={this.toggleChat}>
-          <i className={'fa' + (this.props.isChatHidden && this.props.isUserListHidden
-            ? ' fa-envelope-o transparent'
-            : ' fa-envelope-open-o transparent active')}
-            aria-hidden="true"></i>
-        </ToggleButton>
-      </div>
+      <ToolbarContainer>
+        <ButtonsWrapper>
+          <ToggleButton id="unhide-button" className="toggle-button"
+            isActive={!(this.props.isChatHidden && this.props.isUserListHidden)}
+            onClick={this.toggleChat}>
+            <i className={'fa' + (this.props.isChatHidden && this.props.isUserListHidden
+              ? ' fa-envelope-o transparent'
+              : ' fa-envelope-open-o transparent active')}
+              aria-hidden="true"></i>
+          </ToggleButton>
+          <DownloadModule canvas={this.props.canvas}/>
+        </ButtonsWrapper>
+      </ToolbarContainer>
     )
   }
 }
+
+const ToolbarContainer = styled.div`
+  position: relative;
+  width: 100%;
+  text-align: center;
+`
+
+const ButtonsWrapper = styled.div`
+  width: 700px;
+  height: 154px;
+  margin: 0 auto;
+`
 
 const ToggleButton = styled.button`
   background-color: ${props => props.isActive ? '#312c32' : '#daad86'}
