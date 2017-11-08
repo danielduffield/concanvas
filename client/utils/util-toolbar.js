81439cd5bd9fef1782f3a0db6ae3c80bcbdbd41b
Daniel Duffield <danieljduffield@gmail.com>
2017-11-08 11:36:10 -0800

    Removed the chat and view orient buttons to be broken out into a separate component, similar to the download module. #22

--- client/utils/util-toolbar.js
+++ client/utils/util-toolbar.js
@@ -2,40 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 import { connect } from 'react-redux'
 
+import ChatOrientModule from './chat-orient-module'
 import DownloadModule from './download-module'
 
 class UtilToolbar extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.toggleChat = this.toggleChat.bind(this)
-    this.rotateToolbar = this.rotateToolbar.bind(this)
-  }
-  toggleChat() {
-    this.props.dispatch({ type: 'TOGGLED_CHAT' })
-  }
-  rotateToolbar() {
-    this.props.dispatch({ type: 'UPDATED_TOOLBAR_ORIENTATION' })
-  }
   render() {
     return (
       <ToolbarContainer isHoriz={this.props.isToolbarHoriz}>
         <ButtonsWrapper>
-          <ToggleButton id="unhide-button" className="toggle-button" title="Toggle Chat"
-            isActive={!(this.props.isChatHidden && this.props.isUserListHidden)}
-            onClick={this.toggleChat}>
-            <i className={this.props.isChatHidden && this.props.isUserListHidden
-              ? 'fa fa-envelope-o transparent inactive'
-              : 'fa fa-envelope-o transparent active'}
-              aria-hidden="true"></i>
-          </ToggleButton>
-          <ToggleButton className="toggle-button" title="Toggle Toolbar Position"
-            onClick={this.rotateToolbar} ia-hidden="true">
-            <i className={this.props.isToolbarHoriz
-              ? 'fa fa-rotate-90 fa-window-maximize transparent inactive'
-              : 'fa fa-rotate-180 fa-window-maximize transparent inactive'
-            }></i>
-          </ToggleButton>
+          <ChatOrientModule />
           <DownloadModule canvas={this.props.canvas}/>
         </ButtonsWrapper>
       </ToolbarContainer>
@@ -56,15 +31,8 @@ const ButtonsWrapper = styled.div`
   margin: ${props => props.isHoriz ? '0' : '0 auto'};
 `
 
-const ToggleButton = styled.a`
-  background-color: ${props => props.isActive ? '#312c32' : '#daad86'}
-  background-color: whitesmoke;
-`
-
 function mapStateToProps(state) {
   return {
-    isChatHidden: state.chat.isChatHidden,
-    isUserListHidden: state.chat.isUserListHidden,
     isToolbarHoriz: state.utility.isToolbarHoriz
   }
 }
